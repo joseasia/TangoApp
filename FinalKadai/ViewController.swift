8cb3ddf31384cd6bf87259c678e6a82b6558e8ac
jose_asia <y.s.e0414@gmail.com>
2019-10-16 21:19:04 +0900

    Initial Commit

---
+++ FinalKadai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalKadai
+//
+//  Created by 佐藤　由羽太 on 2019/10/16.
+//  Copyright © 2019 jose_asia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
