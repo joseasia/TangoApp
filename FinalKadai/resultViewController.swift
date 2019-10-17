4cd752d95b68fdd0e9bda27b21dc271cd636e1d3
jose_asia <y.s.e0414@gmail.com>
2019-10-17 09:23:40 +0900

    realm導入、.gitignore作成まで完了

---
+++ FinalKadai/resultViewController.swift
@@ -0,0 +1,33 @@
+//
+//  resultViewController.swift
+//  FinalKadai
+//
+//  Created by 佐藤　由羽太 on 2019/10/17.
+//  Copyright © 2019 jose_asia. All rights reserved.
+//
+
+import UIKit
+
+class resultViewController: UIViewController {
+    
+    @IBOutlet weak var wordLabel1: UILabel!
+    @IBOutlet weak var wordLabel2: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '12.2'
+use_frameworks!
+
+target 'FinalKadai' do
+  pod 'RealmSwift', '3.15.0'
+end
